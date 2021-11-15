6fb38c1df0ac38ebed4473f87b7f68cbf76804fb
Bruno Fialho <brugerf@gmail.com>
2021-11-15 20:43:32 -0300

    All tests done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,37 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks(state => [
+      ...state,
+      {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks: Task[] = tasks
+      .map(task => {
+        if (task.id === id) {
+          return {
+            id,
+            title: task.title,
+            done: !task.done
+          }
+        } else {
+          return task
+        }
+      })
+
+    setTasks(() => updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(() => updatedTasks);
   }
 
   return (
